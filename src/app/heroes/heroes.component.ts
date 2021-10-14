3ee81a287d31e56ba469a7c13a3bdefec42afc6c
yogeshnandwana <yogesh.nandwana@gmail.com>
2021-10-14 17:43:30 +0530

    HeroDetail component routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,17 +10,11 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes:Hero[] = [];
-  selectedHero?: Hero;
   
   constructor(private heroService:HeroService,private msgService:MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.msgService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
